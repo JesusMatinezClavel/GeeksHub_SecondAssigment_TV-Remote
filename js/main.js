69e31887758777a8b49984e5ee621067a6989826
JesusMartinezClavel <jmcvalles@gmail.com>
2024-02-04 17:53:36 +0100

    refactored the code to work better putting everthing inside functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/////////////////////////////////////////////////////////////////////// DECLARACIONES
+
+
 const screen = document.getElementById("tvScreen")
 const chButtons = document.getElementsByClassName("chButton")
 
@@ -7,35 +10,66 @@ let IO = document.getElementById("ioButton")
 
 let originalState = screen.classList.value
 
-let on = false
+let on
 
+
+/////////////////////////////////////////////////////////////////////// FUNCIONES
+
+// FUNCION PARA ENCENDER Y APAGAR LA TV
+function screenOnOff () {
+    screen.classList.toggle("off");
+} 
+
+// FUNCION PARA CAMBIAR EL ESTADO DE ON
 function update() {
     !screen.classList.contains("off")
         ? on = true
         : on = false
 }
 
+// FUNCION PARA DARLE UNA FUNCIONALIDAD A LOS BOTONES DE CANALES
 function buttonClick(e) {
     screen.classList.remove(screen.classList[screen.classList.length - 1])
     screen.classList.add("ch" + e.target.id.slice(-1))
 
 }
 
-IO.addEventListener("click", (e) => {
-    screen.classList.toggle("off");
-    update()
+// FUNCION PARA AÑADIR EL BOTON A LOS CANALES
+function addButtonClick() {
+    buttonsList.map(
+        (item, index) => {
+            item.addEventListener("click", buttonClick)
+        }
+    )
+}
+
+// FUNCION PARA QUITAR EL BOTON DE LOS CANALES
+function removeButtonClick() {
+    buttonsList.map(
+        (item, index) => {
+            item.removeEventListener("click", buttonClick)
+        }
+    )
+}
+
+// FUNCION PARA CONVERTIR LAS ANTERIORES FUNCIONES EN UNA SOLA
+function toggleChannelClick() {
     if (on == true) {
-        buttonsList.map(
-            (item, index) => {
-                item.addEventListener("click", buttonClick)
-            }
-        )
+        addButtonClick()
     } else {
         screen.className = originalState
-        buttonsList.map(
-            (item, index) => {
-                item.removeEventListener("click", buttonClick)
-            }
-        )
+        removeButtonClick()
     }
+}
+
+
+/////////////////////////////////////////////////////////////////////// EVENTOS
+
+
+
+///////////// EVENTO PARA ENCENDER LA TV Y POSIBILITAR EL RESTO DE FUNCIONES
+IO.addEventListener("click", (e) => {
+    screenOnOff()
+    update()
+    toggleChannelClick()
 });
