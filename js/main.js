6f5a8a3dc9251bd26241d311e4468ab529d2789e
JesusMartinezClavel <jmcvalles@gmail.com>
2024-02-05 03:28:43 +0100

    worked on the funcionality of the buttons

--- js/main.js
+++ js/main.js
@@ -3,19 +3,30 @@
 
 const screen = document.getElementById("tvScreen")
 const chButtons = document.getElementsByClassName("chButton")
-const channels = document.getElementsByClassName("channel")
 
-let channelList = Array.from(channels)
+const channel1 = document.getElementById("channel1")
+const channel2 = document.getElementById("channel2")
+const channel3 = document.getElementById("channel3")
+const channel4 = document.getElementById("channel4")
+const channel5 = document.getElementById("channel5")
+const channel6 = document.getElementById("channel6")
+const channel7 = document.getElementById("channel7")
+const channel8 = document.getElementById("channel8")
+const channel9 = document.getElementById("channel9")
+const channel0 = document.getElementById("channel0")
+
+let channelList = [channel1, channel2, channel3, channel4, channel5, channel6, channel7, channel8, channel9, channel0]
 
 let buttonsList = Array.from(chButtons)
 
 let IO = document.getElementById("ioButton")
 
 let originalState = screen.classList.value
+let originalChannelState = channelList.map(channel => channel.classList.value)
 
 let on
 
-console.log(channelList)
+
 /////////////////////////////////////////////////////////////////////// FUNCIONES
 
 // FUNCION PARA ENCENDER Y APAGAR LA TV
@@ -30,10 +41,18 @@ function update() {
         : on = false
 }
 
-// FUNCION PARA DARLE UNA FUNCIONALIDAD A LOS BOTONES DE CANALES
+
+let buttonIndexLast = -1
+
+
+// FUNCION PARA AÑADIR Y QUITAR A LOS CANALES DE LA CLASE INDEX2
 function buttonClick(e) {
-    screen.classList.remove(screen.classList[screen.classList.length - 1])
-    screen.classList.add("ch" + e.target.id.slice(-1))
+    const buttonIndex = buttonsList.indexOf(e.target)
+    if (buttonIndexLast !== -1) {
+        channelList[buttonIndexLast].classList.remove("index2")
+    }
+    channelList[buttonIndex].classList.add("index2");
+    buttonIndexLast = buttonIndex
 }
 
 // FUNCION PARA AÑADIR EL BOTON A LOS CANALES
@@ -60,13 +79,13 @@ function toggleChannelClick() {
         addButtonClick()
     } else {
         screen.className = originalState
+        channelList[buttonIndexLast].className = originalChannelState
         removeButtonClick()
     }
 }
 
 
 /////////////////////////////////////////////////////////////////////// EVENTOS
-
 
 
 ///////////// EVENTO PARA ENCENDER LA TV Y POSIBILITAR EL RESTO DE FUNCIONES
